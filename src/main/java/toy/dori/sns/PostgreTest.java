54a584c5cd0cf5af1bdad0195fed54b323fb613b
dongyeon.jang <zhemaud111@gmail.com>
2024-11-23 22:47:20 +0900

    ignore 설정

--- src/main/java/toy/dori/sns/PostgreTest.java
+++ src/main/java/toy/dori/sns/PostgreTest.java
@@ -30,6 +30,6 @@ public class PostgreTest implements ApplicationRunner {
             String sql = "CREATE TABLE t_product(product_no INTEGER NOT NULL, product_name VARCHAR(255), PRIMARY KEY (product_no))";
             statement.executeUpdate(sql);
         }
-        jdbcTemplate.execute("INSERT INTO t_product VALUES (1, 'Big shirt')");
+        jdbcTemplate.execute("INSERT INTO t_product VALUES (1, 'Big shir')");
     }
 }
